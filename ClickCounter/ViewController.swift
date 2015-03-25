8a767061fbada7a8b5816dbbd1f7fa21147c8332
John Hogarty <hogihung@gmail.com>
2015-03-24 21:11:25 -0400

    Refactor to use storyboards.

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -11,44 +11,19 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Setup for our label
-        var label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
-        
-        self.view.addSubview(label)
-        self.label = label
-        
-        // Setup for our increment button
-        var incrementButton = UIButton()
-        incrementButton.frame = CGRectMake(80, 250, 80, 180)
-        incrementButton.setTitle("+", forState: .Normal)
-        incrementButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(incrementButton)
-        
-        incrementButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        // Setup for our decrement button
-        var decrementButton = UIButton()
-        decrementButton.frame = CGRectMake(240, 250, 80, 180)
-        decrementButton.setTitle("-", forState: .Normal)
-        decrementButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(decrementButton)
-        
-        decrementButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count++
         self.label.text = "\(self.count)"
     }
     
-    func decrementCount() {
+    @IBAction func decrementCount() {
         self.count--
         self.label.text = "\(self.count)"
     }
